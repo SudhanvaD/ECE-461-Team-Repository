622aadbce3087bfd91057ea2680781201784f47a
Haani Tharak <haanitharak@gmail.com>
2023-01-30 15:05:23 -0500

    Some changes made to API file

--- API.py
+++ API.py
@@ -1,21 +1,29 @@
 import requests
 import json
 
-## Get number of contributors
+"""https://github.com/cloudinary/cloudinary_npm
+https://www.npmjs.com/package/express
+https://github.com/nullivex/nodist
+https://github.com/lodash/lodash
+https://www.npmjs.com/package/browserify"""
+
+# Get number of contributors
 url = 'https://api.github.com/repos/namruthahari/Sample-Git-Repo/contributors'
 response = requests.get(url)
 rd = response.json()
 num_contributors = len(rd)
 
-url = 'https://api.github.com/repos/namruthahari/Sample-Git-Repo/issues'
+url = 'https://github.com/nullivex/nodist'
 response = requests.get(url)
 rd = response.json()
+print(rd)
+print(rd[0]['url'])
 
-print(rd['issue_events_url'])
-print(rd['issue_comment_url'])
-print(rd['issues_url'])
-print(rd['has_issues'])
-print(rd['open_issues_count'])
-print(rd['open_issues'])
-
-
+"""
+print(rd[0]['issue_events_url'])
+print(rd[0]['issue_comment_url'])
+print(rd[0]['issues_url'])
+print(rd[0]['has_issues'])
+print(rd[0]['open_issues_count'])
+print(rd[0]['open_issues'])
+"""

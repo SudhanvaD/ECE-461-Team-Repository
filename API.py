0af86a1b5b3139ba147b98425b6df1fcbc6720c3
Jack Knox <knox36@purdue.edu>
2023-02-01 22:00:51 -0500

    API updates with formatted inputs and outputs #8

--- API.py
+++ API.py
@@ -2,9 +2,25 @@ import requests
 import json
 
 ## Get number of contributors
+
 url1 = 'https://api.github.com/repos/cloudinary/cloudinary_npm'
 url2 = 'https://github.com/nullivex/nodist'
 url3 = 'https://github.com/lodash/lodash'
+
+file = open(r'SampleUrlFile.txt',"r")
+input = file.readlines()
+file.close()
+input[len(input)-1] = input[len(input)-1] + '\n'
+input = [i[:-1] for i in input]
+git_inputs = [i for i in input if i[8:11] == 'git']
+npm_inputs = [i for i in input if i[8:11] == 'npm']
+
+def format_url(url):
+    url = url.split('://')
+    url = url[0] + '://api.' + url[1]
+    url = url.split('.com/')
+    url = url[0] + '.com/repos/' + url[1]
+    return url
 
 ## If username and token are provided then will authorize, can be adjusted as neccesary
 def authorize(username,token):
@@ -93,7 +109,7 @@ def license(url,username = False,token = False):
     if 'license' in rd:
         return(rd['license']['name'])
     else:
-        return None
+        return 'None'
 
 ## Returns a health percentage score based on if there is a readme, contributing, license, and code of conduct
 ## Can be edited to retrieve any of these
@@ -102,6 +118,7 @@ def Community_Metrics(url,username = False,token = False):
     
     response = requests.get(url,auth = authorize(username,token))
     rd = response.json()
+    print(rd)
     return rd['health_percentage']
 
 ## Returns the number of closed issues to a repo, can do up to 100
@@ -117,8 +134,26 @@ def pull_requests(url,username = False,token = False):
     rd = response.json()
     return len(rd)
 
+def write(input):
+    out = open(r'out.txt','w')
+    for url in input:
+        url = format_url(url)
+        print(url)
+        out.write(url + '\n')
+        out.write('Number of Events: ' + str(events(url)) + '\n')
+        out.write('Number of Starred: ' + str(starred(url)) + '\n')
+        out.write('Number of Subscribers: ' + str(subscribers(url)) + '\n')
+        out.write('Number of Commits: ' + str(commits(url)) + '\n')
+        out.write('Number of Open_Issues: ' + str(open_issues(url)) + '\n')
+        out.write('Number of Closed_Issues: ' + str(closed_issues(url)) + '\n')
+        out.write('License: ' + license(url) + '\n')
+        out.write('Community Metric: ' + str(Community_Metrics(url)) + '\n')
+        out.write('Pull_Requests: ' + str(pull_requests(url)) + '\n')
+        out.write('\n')
+    out.close()
 
 
+write(git_inputs)
 
 
 

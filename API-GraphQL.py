bb48eb51248b6e38c81ca90eb0ed31f341fa0357
Haani Tharak <haanitharak@gmail.com>
2023-02-08 17:18:54 -0500

    GraphQL GitHub for Rate Limit - Finished

--- API-GraphQL.py
+++ API-GraphQL.py
@@ -1,1 +1,35 @@
+# An example to get the remaining rate limit using the Github GraphQL API.
 
+import requests
+
+headers = {"Authorization": "Bearer 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"}
+
+
+def run_query(query):  # A simple function to use requests.post to make the API call. Note the json= section.
+    request = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
+    if request.status_code == 200:
+        return request.json()
+    else:
+        raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
+
+
+# The GraphQL query (with a few aditional bits included) itself defined as a multi-line string.
+query = """
+{
+  viewer {
+    login
+  }
+  rateLimit {
+    limit
+    cost
+    remaining
+    resetAt
+  }
+}
+"""
+
+
+
+result = run_query(query)  # Execute the query
+remaining_rate_limit = result["data"]["rateLimit"]["remaining"]  # Drill down the dictionary
+print("Remaining rate limit - {}".format(remaining_rate_limit))

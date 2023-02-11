ff18170a6eba18759f0908a5210609040407cfd7
Haani Tharak <haanitharak@gmail.com>
2023-02-11 18:48:27 -0500

    Major changes to API-GraphQL.py

--- API-GraphQL.py
+++ API-GraphQL.py
@@ -1,10 +1,47 @@
 import requests
 from decouple import config
+import urlParser
+import re
+from urllib.parse import urlparse
+
 
 token = config('API_KEY')
 
 headers = {"Authorization": 'Bearer ' + token}
 
+def generateQuery(url):
+
+    parseResults = urlparse(url)
+    path = parseResults.path
+    repoDetails = path.split('/')
+    owner = '"' + repoDetails[1] + '"'
+    name = '"' + repoDetails[2] + '"'
+
+
+    graphQlQuery = """
+    query {
+      repositoryOwner (login: """+owner+""") {
+        repository(name: """+name+""") {
+          forks {
+            totalCount
+          }
+          issues {
+            totalCount
+          }
+          stargazers {
+            totalCount
+          }
+          watchers {
+            totalCount
+          }
+          pullRequests {
+            totalCount
+          }
+        }
+      }
+    }"""
+    #print(graphQlQuery)
+    return graphQlQuery
 
 def run_graphQlQuery(graphQlQuery):  # A simple function to use requests.post to make the API call. Note the json= section.
     request = requests.post('https://api.github.com/graphql', json={'query': graphQlQuery}, headers=headers)
@@ -14,24 +51,14 @@ def run_graphQlQuery(graphQlQuery):  # A simple function to use requests.post to
         raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, graphQlQuery))
 
 
-# The GraphQL query (with a few aditional bits included) itself defined as a multi-line string.
-graphQlQuery = """
-{
-  viewer {
-    login
-  }
-  rateLimit {
-    limit
-    cost
-    remaining
-    resetAt
-  }
-}
-"""
+graphQlData = []
+for url in urlParser.validUrls:
+    query = generateQuery(url)
+    result = run_graphQlQuery(query)
+    graphQlData.append(result['data']['repositoryOwner']['repository'])
 
-result = run_graphQlQuery(graphQlQuery)  # Execute the query
-remaining_rate_limit = result["data"]["rateLimit"]["remaining"]  # Drill down the dictionary
-print("Remaining rate limit - {}".format(remaining_rate_limit))
+#Printing and Saving to text file
+for i in graphQlData: print("Info - {}".format(i))
 
 with open("outputGraphQl.txt", "w") as f:
-    print("Remaining rate limit - {}".format(remaining_rate_limit), file=f)
+    for i in graphQlData: print("Info - {}\n".format(i), file=f)

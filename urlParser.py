cdfb303e23bfe1cc1698165a80bf11d2b478f6ec
Jack Knox <knox36@purdue.edu>
2023-02-12 15:49:47 -0500

    Update urlParser.py

--- urlParser.py
+++ urlParser.py
@@ -1,7 +1,14 @@
 import validators
 from npmAPI import findGitUrl
+import git
 
 #urlFilePath = "/Users/haani/Documents/Spring 2023/ECE 461/Haani's Fork/ECE-461-Haani-Repository/Url File.txt"
+
+def clone(url):
+    s = url.split("/").pop()
+    local_path = s
+    #os.makedirs(s)
+    git.Repo.clone_from(url, local_path)
 
 def parseUrls(urlFilePath):
     try:
@@ -16,11 +23,20 @@ def urlValidator(urls):
     for url in urls:
         url = url.replace('\n','')
         if validators.url(url) is True and 'github.com' in url:
-            print(url)
+            try:
+                clone(url)
+            except:
+                print('No Access')
+                # log that repo could not be cloned
             validatedUrls.append(url)
         elif validators.url(url) is True and 'www.npmjs.com' in url:
             print(url)
             gitUrl = findGitUrl(url)
+            try:
+                clone(gitUrl)
+            except:
+                print('No Access')
+                 # Log that repo could not be cloned
             validatedUrls.append(gitUrl)
 
     for i in range(len(validatedUrls)):

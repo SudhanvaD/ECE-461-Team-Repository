bc70075e9a3eca8377c08d5db3a9ebd40f512322
SalimAlSalmi <78392443+SalimAlSalmi@users.noreply.github.com>
2023-02-12 16:33:52 -0500

    new functions added

--- testing.py
+++ testing.py
@@ -38,4 +38,61 @@ def compare_open_issues(hardcoded_value):
         return "The value of open issues is equal to the hardcoded value of %d." % hardcoded_value
     else:
         return "The value of open issues is not equal to the hardcoded value of %d." % hardcoded_value
+def compare_Closed_Issues(hardcoded_value):
+    # Get the open issues from the API
+    closed_Issues = get_closed_issues_from_api()
+    
+    if closed_issues == hardcoded_value:
+        return "The value of open issues is equal to the hardcoded value of %d." % hardcoded_value
+    else:
+        return "The value of open issues is not equal to the hardcoded value of %d." % hardcoded_value
+
+def compare_Closed_Issues(hardcoded_value):
+    # Get the open issues from the API
+    closed_Issues = get_closed_issues_from_api()
+    
+    if closed_issues == hardcoded_value:
+        return "The value of open issues is equal to the hardcoded value of %d." % hardcoded_value
+    else:
+        return "The value of open issues is not equal to the hardcoded value of %d." % hardcoded_value
+def compare_commits(hardcoded_value):
+    # Get the open issues from the API
+    commits = get_commits_from_api()
+    
+    if commits == hardcoded_value:
+        return "The value of open issues is equal to the hardcoded value of %d." % hardcoded_value
+    else:
+        return "The value of open issues is not equal to the hardcoded value of %d." % hardcoded_value
+def compare_Subscribers(hardcoded_value):
+    # Get the open issues from the API
+    subscribers = get_subscribers_from_api()
+    
+    if subscribers == hardcoded_value:
+        return "The value of open issues is equal to the hardcoded value of %d." % hardcoded_value
+    else:
+        return "The value of open issues is not equal to the hardcoded value of %d." % hardcoded_value
+def compare_Events(hardcoded_value):
+    # Get the open issues from the API
+    events = get_events_from_api()
+    
+    if events == hardcoded_value:
+        return "The value of open issues is equal to the hardcoded value of %d." % hardcoded_value
+    else:
+        return "The value of open issues is not equal to the hardcoded value of %d." % hardcoded_value
+def compare_Starred(hardcoded_value):
+    # Get the open issues from the API
+    starred = get_starred_from_api()
+    
+    if starred == hardcoded_value:
+        return "The value of open issues is equal to the hardcoded value of %d." % hardcoded_value
+    else:
+        return "The value of open issues is not equal to the hardcoded value of %d." % hardcoded_value
+def compare_Events(hardcoded_value):
+    # Get the open issues from the API
+    events = get_events_from_api()
+    
+    if events == hardcoded_value:
+        return "The value of open issues is equal to the hardcoded value of %d." % hardcoded_value
+    else:
+        return "The value of open issues is not equal to the hardcoded value of %d." % hardcoded_value
 

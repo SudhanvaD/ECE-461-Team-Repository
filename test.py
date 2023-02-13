1d8a2120e473e826b17d9714f540bbd0f04b7703
SalimAlSalmi <78392443+SalimAlSalmi@users.noreply.github.com>
2023-02-12 19:27:32 -0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,43 @@
+# Read from file
+file = "out.txt"
+with open(file, 'r') as f:
+    data = f.read()
+
+# Split the data into lines
+lines = data.split("\n")
+
+# Assign values to variables
+Url = lines[0]
+Number_of_Events = (lines[1])
+Number_of_Starred = (lines[2])
+Number_of_Subscribers = (lines[3])
+Number_of_Commits = (lines[4])
+Number_of_Open_Issues = (lines[5])
+Number_of_Closed_Issues = (lines[6])
+License = lines[7]
+Community_Metric = (lines[8])
+Pull_Requests = (lines[9])
+
+
+file_expected = "out_expected.txt"
+with open(file_expected, 'r') as f:
+    data_expected = f.read()
+
+# Split the data into lines
+lines_expected = data_expected.split("\n")
+
+# Assign values to variables
+Url_expected = lines[0]
+Number_of_Events_expected = (lines[1])
+Number_of_Starred_expected = (lines[2])
+Number_of_Subscribers_expected = (lines[3])
+Number_of_Commits_expected = (lines[4])
+Number_of_Open_Issues_expected = (lines[5])
+Number_of_Closed_Issues_expected = (lines[6])
+License_expected = lines[7]
+Community_Metric_expected = (lines[8])
+Pull_Requests_expected = (lines[9])
+
+for i in range(len(lines)):
+    assert lines[i] == lines_expected[i], f"Mismatch in line {i + 1}: expected '{lines_expected[i]}' but got '{lines[i]}'"
+    print(f"Match in line {i + 1}: '{lines[i]}'")
